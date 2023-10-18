e519a86d0bbefe99471b3c5458ba620922a86258
bishal-dd <dhakalbishal930@gmail.com>
2023-10-19 04:43:36 +0600

    media removed

--- webapp/urls.py
+++ webapp/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
 
 # Add this at the end of your urlpatterns
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
