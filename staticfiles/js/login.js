740bb19dfc5cd0de9e490b82b91d21b153d7395f
bishal-dd <dhakalbishal930@gmail.com>
2023-10-18 23:49:45 +0600

    static files

---
+++ staticfiles/js/login.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // START LOGIN POPUP WINDOW
+  $("#googleLink").on("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior
+
+    // URL to open in the popup
+    var url = $(this).attr("href");
+
+    // Define the dimensions of the popup window
+    var width = 400;
+    var height = 400;
+
+    // Calculate the position of the popup in the center of the screen
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    // Open the popup window
+    window.open(
+      url,
+      "Popup",
+      "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top
+    );
+
+    // If the login is successful, close the window
+    if (loginSuccessful) {
+      window.close();
+    }
+  });
+
+  // END LOGIN POPUP WINDOW
+});
---
+++ staticfiles/js/validation.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+  // Try showing the modal directly
+  function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+    const day = today.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  function validate() {
+    if ($("#recipient_phone").val() === "") {
+      // Display the Bootstrap modal using jQuery
+      $("#validationModal").modal("show");
+      $("#validationErrorMessage").text("Phone number cannot be empty.");
+      return false;
+    } else if ($("#recipient_name").val() === "") {
+      // Display the Bootstrap modal using jQuery
+      $("#validationModal").modal("show");
+      $("#validationErrorMessage").text("Name cannot be empty.");
+      return false;
+    } else if ($("#amount").val() === "") {
+      // Display the Bootstrap modal using jQuery
+      $("#validationModal").modal("show");
+      $("#validationErrorMessage").text("Name cannot be empty.");
+      return false;
+    } else if (
+      $("#paymentMode").val() === "Bank Transfer" &&
+      $("#journalNumber").val() === ""
+    ) {
+      // Display the Bootstrap modal using jQuery
+      $("#validationModal").modal("show");
+      $("#validationErrorMessage").text("Journal Number cannot be empty.");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  $("#generate_pdf").on("click", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    if (validate()) {
+      $("#data_form").submit();
+         setTimeout(function() {
+            window.location.href = "/";
+         }, 2000);
+    }
+  });
+
+  $("#save_data").on("click", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    // Create a FormData object to handle the form data
+    var formData = new FormData($("form")[0]);
+
+    // Append the image file to the FormData object
+    var logoImageInput = $("#logo_edit").val;
+    var logoImagenew = $("#logo_upload").val;
+    if (logoImageInput) {
+      formData.append("logo_image", logoImageInput);
+    } else if (logoImagenew) {
+      formData.append("logo_image", logoImagenew);
+    }
+
+    var signInput = $("#signature_edit").val;
+    var signInputNew = $("#signature_upload").val;
+    if (signInput) {
+      formData.append("signature_image", signInput);
+    } else if (signInputNew) {
+      formData.append("signature_image", signInputNew);
+    }
+
+    if (validate()) {
+      $.ajax({
+        type: "POST",
+        url: "/save_receipt/", // Replace with your actual URL
+        data: formData,
+        processData: false,
+        contentType: false,
+        cache: false,
+        enctype: "multipart/form-data",
+        success: function (response) {
+        if(response.message === "Trial attempts up")
+            {
+                window.open("/trial_period/", "_blank");
+            }
+        else
+        {
+              $("#successModal").modal("show");
+              $("#validationSuccessMessage").text("Your data was saved");
+               setTimeout(function() {
+                    window.location.href = "/";
+               }, 2000);
+
+        }
+
+        },
+        error: function (error) {
+          // Handle any errors that occur during the AJAX request
+          alert("Error while saving data: " + error.responseText);
+        },
+      });
+    }
+  });
+});
